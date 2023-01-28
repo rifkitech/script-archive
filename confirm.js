71731425c9fd6a927f2585a3a464040568f3291c
MUHAMMAD RIFKI <112795137+rifkitech@users.noreply.github.com>
2023-01-28 19:49:25 +0700

    Create confirm.js

---
+++ confirm.js
@@ -0,0 +1,9 @@
+<script>
+        var yakin = confirm("Situs ini dilindungi oleh Cloudflare, Domain yang digunakan gratis dari https://nic.eu.org. Klik Oke untuk melanjutkan");
+
+        if (yakin) {
+            window.location = "https://example.com";
+        } else {
+            document.write("Baiklah, refresh halaman ini jika anda tidak sengaja memilih Cancel / Batal");
+        }
+    </script>
