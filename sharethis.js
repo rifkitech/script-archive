8846080e26a676178b725b254ec6e6253ec06b6a
MUHAMMAD RIFKI <muhammadrifkibelajar05@gmail.com>
2023-02-08 20:17:50 +0700

    Create sharethis.js

---
+++ sharethis.js
@@ -0,0 +1,6 @@
+//Note: Script ini telah di parse, mungkin tidak akan berfungsi pada Website dengan file HTML terpisah (Berhasil saat digunakan di Blogger)
+
+&lt;script type=&#039;text/javascript&#039; 
+src=&#039;https://platform-api.sharethis.com/js/sharethis.js#property=634869474fbf4700192d3a89&amp;product=powr-social-feed&#039; async=&#039;async&#039;&gt;&lt;/script&gt;
+
+<div class="sharethis-powr-social-feed"></div>
